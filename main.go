206fedabfa9d26509be0d51996f0a430b746b91c
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 11:45:51 +0100

    GPIO test

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"fmt"
-	src "github.com/tkuehn1/Studienarbeit_src"
-)
+import src "github.com/tkuehn1/Studienarbeit_src"
 
 func main() {
-	//go src.Tcp_conn()
-	src.gpio()
-	//src.Tcp_client()
-
-	//fmt.Println("hello")
+	src.Gpio()
 }
